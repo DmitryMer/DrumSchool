5496bff1096d1325dea495c98cf7a1c0361d29f2
DmirtyMer <duduff9317@gmail.com>
2020-04-23 10:48:13 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,49 @@
+   function change() {
+	 	var chbox; 
+		chbox = document.getElementById('checkBox');
+
+		var colorTxt;
+		colorTxt = document.getElementById('colorTxt');
+		
+		if (chbox.checked) {
+			colorTxt.style.color = "#00cc99";
+		} 	else {
+			colorTxt.style.color = "#000";
+		}
+	};
+
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlide() {
+  showSlides(slideIndex += 1);
+}
+
+function minusSlide() {
+  showSlides(slideIndex -= 1);  
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("parag");
+  var dots = document.getElementsByClassName("noguma-pomoki-kadra");
+  if (n > slides.length) {
+  slideIndex = 1
+  }
+  if (n < 1) {
+  slideIndex = slides.length
+  }
+  for (i = 0; i < slides.length; i++) {
+  slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+  dots[i].className = dots[i].className.replace("deystvuyus", "");
+  }
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " deystvuyus";
+}
